164db8fbc1c1a36ca4a351db8053ddd33e77d49d
Rahul Padole <159533975+rahulpadole@users.noreply.github.com>
2025-07-19 16:13:21 +0530

    Update db.php
    Change as per the deployment

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,15 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'login_system';
 
-// Create a new connection to the MySQL database
-$conn = new mysqli($host, $user, $password, $dbname);
+$host = "sql209.infinityfree.com"; // Found in control panel (e.g., sql204.epizy.com)
+$dbname = "if0_39508444_login_system"; // Your database name
+$username = "if0_39508444"; // Your InfinityFree DB username
+$password = "Rahul1968"; // Set in control panel
 
-// Check if the connection was successful
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
